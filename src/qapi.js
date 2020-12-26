4267fa35a363612ab85a27c2a71ae6a33bd33890
YogPanjarale <64301340+YogPanjarale@users.noreply.github.com>
2020-12-26 18:00:16 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fe7289b94657a0021e7d92a",
+				"5fe7289b94657a0021e7d92c"
+			]
+		},
+		"5fe7289b94657a0021e7d92a": {
+			"id": "5fe7289b94657a0021e7d92a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fe7289b94657a0021e7d92c": {
+			"id": "5fe7289b94657a0021e7d92c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
